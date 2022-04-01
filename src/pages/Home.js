4a55daa2a12cb78ade10254f38c65a9f77e87706
Inez Yulia Amanda <inez.amanda16@gmail.com>
2022-04-01 22:14:11 +0700

    install react-router, react-router-dom, axios

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section className="min-h-screen ">
+      <div className="layout">
+        <img src="./images/img_car.png" />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {
-          colors: {
-            blue: {
-              primary: '#0D28A6',
-              secondary: '#F1F3FF',
-            }
-          }
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          primary: "#0D28A6",
+          secondary: "#F1F3FF",
         },
+        green: {
+          primary: "#5CB85F",
+          secondary: '#C9E7CA'
+        },
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
