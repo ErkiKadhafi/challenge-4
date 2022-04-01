02038c62fe059d7de7ff1359ad704729adb1facf
ErkiKadhafi <erki_rosyid@igrow.asia>
2022-04-01 21:33:54 +0700

    git init, install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+          colors: {
+            blue: {
+              primary: '#0D28A6',
+              secondary: '#F1F3FF',
+            }
+          }
+        },
+    },
+    plugins: [],
+};
